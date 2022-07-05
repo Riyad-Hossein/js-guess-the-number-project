adb12ce6a30709a038185794a8fa5b319129209d
“Riyad-Hossein” <riyadhossein@gmail.com>
2022-07-05 14:32:17 +0600

    js file added

---
+++ app.js
@@ -0,0 +1,6 @@
+const randomNumber = Math.floor((Math.random() * 10) + 1);
+const guessNumber = document.getElementById("guessNumber");
+const inputNumber = document.getElementById("inputNumber");
+const guessText = document.getElementById("guessText");
+const scoreText = document.getElementById("scoreText");
+const highScoreText = document.getElementById("highScoreText");
